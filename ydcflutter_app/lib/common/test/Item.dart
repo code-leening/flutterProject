3669da14221bc068e833587dc7b4dc8e56f95e3c
lining <1041204250@qq.com>
2019-12-12 16:40:01 +0800

    delete

--- ydcflutter_app/lib/common/test/Item.dart
+++
@@ -1,5 +0,0 @@
-class Item {
-  Item(this.price, this.count);
-  double price; //商品单价
-  int count; // 商品份数
-}
--- ydcflutter_app/lib/eventbus/EventParam.dart
+++
@@ -1,7 +0,0 @@
-class EventParam{
-   String id;
-   String name;
-   EventParam(this.id,this.name);
-   String get getName => this.name;
-   String get getId=>this.id;
-}
